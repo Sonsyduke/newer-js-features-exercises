204d3951b67e6f381ef670f5d336ebc89f7ce90f
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-16 15:48:03 +0200

    Combining two arrays

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ const nums = [12, 23, 45, 222, 664, 234, 1092, 3482, 453, 212];
 
 const highestNum = Math.max(...nums);
 const lowestNum = Math.min(...nums);
+
+// COMBINING TWO ARRAYS
+const dogs = ["Rocky", "Rolly", "Bruno"];
+const cats = ["Matt", "Ginger"];
+
+const allPets = [...dogs, ...cats];
