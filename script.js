723a8312a2379b7423290d5906bcc0cab51dfd5c
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-16 15:58:46 +0200

    Combining two objects

--- script.js
+++ script.js
@@ -8,13 +8,28 @@
 // }
 
 // SPREAD
-const nums = [12, 23, 45, 222, 664, 234, 1092, 3482, 453, 212];
+// const nums = [12, 23, 45, 222, 664, 234, 1092, 3482, 453, 212];
 
-const highestNum = Math.max(...nums);
-const lowestNum = Math.min(...nums);
+// const highestNum = Math.max(...nums);
+// const lowestNum = Math.min(...nums);
 
 // COMBINING TWO ARRAYS
-const dogs = ["Rocky", "Rolly", "Bruno"];
-const cats = ["Matt", "Ginger"];
+// const dogs = ["Rocky", "Rolly", "Bruno"];
+// const cats = ["Matt", "Ginger"];
 
-const allPets = [...dogs, ...cats];
+// const allPets = [...dogs, ...cats];
+
+// SPREAD ON OBJECTS
+const dog = {
+  name: "Rolly",
+  age: 1,
+  type: "dog",
+};
+
+const cat = {
+  name: "Matt",
+  age: 5,
+  type: "cat",
+};
+
+const myPets = {...dog, ...cat};
